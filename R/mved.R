e4d0e7a9413b10c07602047e8afa16150d352f67
dyphen12 <alexiswong10@gmail.com>
2021-08-01 02:54:35 -0400

    Updated mved

--- R/mved.R
+++ R/mved.R
@@ -19,6 +19,7 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+library(forecast)
 
 
 univariate <- function(x) {
@@ -40,9 +41,9 @@ univariate <- function(x) {
 
   # Data Load
 
-  tsearn = x
+  process = x
 
-  fit <- Arima(tsearn, order=c(1,0,1))
+  fit <- Arima(process, order=c(1,0,1))
 
   res = resid(fit)
 
@@ -50,22 +51,24 @@ univariate <- function(x) {
 
   #RW
 
-  tam = nrow(tsearn)
+  tam = nrow(process)
 
-  RW <- arima.sim(list(ar=fit3$coef["ar1"],ma=fit$coef["ma1"]),tam)
+  RW <- arima.sim(list(ar=fit$coef["ar1"],ma=fit$coef["ma1"]),tam)
 
   RW_diff <- diff(RW)
 
-  RW_drift <- arima.sim(model= list(ar=fit3$coef["ar1"],ma=fit$coef["ma1"]), n=tam, mean=1,sd=5)
+  RW_drift <- arima.sim(model= list(ar=fit$coef["ar1"],ma=fit$coef["ma1"]), n=tam, mean=1,sd=5)
 
-  prePit = data.frame(tsearn,RW_drift)
-  postPit = prePit %>% replace(is.na(.), 0) %>% mutate(Peet = rowSums(.[1:2]))
+  prePit = data.frame(process,RW_drift)
+  postPit = prePit %>% replace(is.na(.), 0) %>% mutate(Pit = rowSums(.[1:2]))
 
   Pit = postPit[3]
 
-  F_temp <- modeldata
+  modeldata = data.frame(Pit,Tit)
+  ymodeldata = modeldata %>% replace(is.na(.), 0) %>% mutate(Yit = rowSums(.[1:2]))
+  Decomposition = ymodeldata[1:3]
 
-  return(Pit,Tit);
+  return(ymodeldata);
 
 }
 
@@ -143,36 +146,117 @@ multivariate <- function(x) {
 
 }
 
-edequation <- function(x, y){
+mvedequation <- function(Pit, Tit){
 
+  Yit <- Pit + Tit
 
+  F_temp <- Yit
+  return(F_temp);
 
 }
 
-permanentcomp <- function(x){
+uvedequation <- function(Pit, Tit){
 
+  modeldata = data.frame(Pit,Tit)
+  ymodeldata = modeldata %>% replace(is.na(.), 0) %>% mutate(Yit = rowSums(.[1:2]))
+  Decomposition = ymodeldata[1:3]
+
+  return(ymodeldata);
+
+}
+
+transitorycomp <- function(x){
 
   library(marima)
 
-  # Transitory Component:
+  dataPR <- x
 
-  # Assuming low order MARIMA
+  tokvar <- ncol(dataPR)
 
-  fitMARIMA <- marima(x)
+  Model1 <- define.model(kvar=tokvar, ar=c(1, 0), ma=c(1), reg.var=13)
+
+  fitMARIMA <- marima(dataPR, ar.pattern = Model1$coef["ar1"], ma.pattern = Model1$coef["ma1"])
 
   resMARIMA = resid(fitMARIMA)
 
   preTit = resMARIMA
 
-  # Resultant Transitory Component
+  # Transitory Component
 
   Tit <- as.data.frame(t(preTit))
 
-  plot.ts(Tit)
-
   return(Tit);
 
+}
 
+permanentcomp <- function(x){
+
+  # Given the equation
+
+  # yit = αi + pit + τit
+
+  # pit = φppit−1 + ξit,
+
+  # τit = θ(L)it
+
+  # yit is individual i’s log-earnings (residuals) at time t;
+  # pit is the permanent component (random walk if φp = 1);
+  # τit is the transitory component: MA(1), ARMA(1,1),
+  # AR(1), or iid;
+  # αi is an individual fixed effect
+
+
+  # Data Load
+
+  # MARIMA Model
+  #
+  # 1) We train an MARIMA automatic model to obtain a full AR and MA coefficients of the multi-variate process.
+  #
+  # 2) Then, we train an ARIMA (1,0,1) model to obtain the resources of this model as seen in the paper.
+  #
+  # 3) We build an autoARIMA + ARIMA (1,0,1) model.
+  #
+  # 4) We have that a model of the permanent effect of the shock is equivalent to a Random Walk
+  #    model first order of differentiation,
+  #    we simulate this with the previously obtained model and thus we obtain this resource.
+
+  #Multi-variate Implementation MARIMA
+
+  library(marima)
+
+  dataPR <- x
+
+  tokvar <- ncol(dataPR)
+
+  Model1 <- define.model(kvar=tokvar, ar=c(1, 0), ma=c(1), reg.var=13)
+
+  fitMARIMA <- marima(dataPR, ar.pattern = Model1$coef["ar1"], ma.pattern = Model1$coef["ma1"])
+
+  resMARIMA = resid(fitMARIMA)
+
+  preTit = resMARIMA
+
+  # Transitory Component
+
+  Tit <- as.data.frame(t(preTit))
+
+  tam = nrow(Tit)
+
+  tam = tam+1
+
+  #Random Walk
+
+  RWmodel <- marima.sim(kvar = tokvar, ar.model = fitMARIMA$coef["ar0"], ma.model = fitMARIMA$coef["ma0"], nsim = tam,  averages = fitMARIMA$averages)
+
+  RW <- diff(RWmodel)
+
+  #Permanent Component
+
+  Pit <- RW
+
+  # Earning Dynamics Equation
+
+  return(Pit);
 
 }
 
